1a91a6877ce870fe568b6134ace9387fb8963f79
Daniel L. Miranda <danieldelacerdamiranda@proton.me>
2024-03-05 22:18:38 -0300

    feat: adds module `index.js` file

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import next from './configs/next.js';
+import node from './configs/node.js';
+import react from './configs/react.js';
+
+export default {
+  configs: {
+    node,
+    react,
+    next,
+  },
+};
