892a1cd0fc55de4708748b10b861b9195e0dd8f1
Daniel L. Miranda <danieldelacerdamiranda@proton.me>
2024-03-20 21:25:05 -0300

    fix: adds `FlatCompat` utility to fix `next` config not working

--- src/configs/next.js
+++ src/configs/next.js
@@ -1,13 +1,14 @@
-import next from '@next/eslint-plugin-next';
+import { FlatCompat } from "@eslint/eslintrc";
+import path from "path";
 import tseslint from 'typescript-eslint';
+import { fileURLToPath } from "url";
 import react from './react.js';
 
-export default tseslint.config(...react, {
-  plugins: {
-    '@next/next': next,
-  },
-  rules: {
-    ...next.configs.recommended.rules,
-    ...next.configs['core-web-vitals'].rules,
-  },
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname
 });
+
+export default tseslint.config(...react, ...compat.extends('plugin:@next/next/recommended'));
