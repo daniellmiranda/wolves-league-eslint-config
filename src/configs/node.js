4b08b160aaff44fd8d91fa6944995f9d147c68cc
Daniel L. Miranda <danieldelacerdamiranda@proton.me>
2024-03-23 22:20:27 -0300

    refactor: simplifies `react` and `node` configs

--- src/configs/node.js
+++ src/configs/node.js
@@ -4,7 +4,6 @@ import tseslint from 'typescript-eslint';
 import base from './base.js';
 
 export default tseslint.config(...base, {
-  files: ['**/*.{js,mjs,cjs,ts}'],
   languageOptions: {
     globals: { ...globals.node },
   },
--- src/configs/react.js
+++ src/configs/react.js
@@ -1,5 +1,4 @@
 import jsxA11y from 'eslint-plugin-jsx-a11y';
-import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
@@ -7,28 +6,14 @@ import tseslint from 'typescript-eslint';
 import base from './base.js';
 
 export default tseslint.config(...base, {
-  files: ['**/*.{js,jsx,ts,tsx}'],
-  plugins: {
-    react,
-    'jsx-a11y': jsxA11y,
-    'react-hooks': reactHooks,
-  },
-  languageOptions: {
-    globals: { ...globals.browser },
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-    },
-  },
+  plugins: { 'react-hooks': reactHooks, 'jsx-a11y': jsxA11y },
   rules: {
-    ...react.configs.recommended.rules,
     ...reactHooks.configs.recommended.rules,
     ...jsxA11y.configs.recommended.rules,
   },
-  settings: {
-    react: {
-      version: 'detect',
+  languageOptions: {
+    globals: {
+      ...globals.browser,
     },
   },
 });
