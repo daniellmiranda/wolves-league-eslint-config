eb459fac456d9ea19742f01e20d4348290bb73c7
Daniel L. Miranda <danieldelacerdamiranda@proton.me>
2024-05-11 15:56:18 -0300

    feat: adds support for commonjs

---
+++ rollup.config.mjs
@@ -0,0 +1,18 @@
+import terser from '@rollup/plugin-terser';
+
+export default {
+  input: './src/index.js',
+  output: [
+    {
+      file: 'dist/index.mjs',
+      format: 'es'
+    },
+    {
+      file: 'dist/index.cjs',
+      format: 'cjs'
+    }
+  ],
+  plugins: [
+    terser(),
+  ]
+};
